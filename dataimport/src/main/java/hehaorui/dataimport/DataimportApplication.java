190bd0c357cc94156fd24ea68a52746271ec6382
hehaorui <hehaourui@outlook.com>
2021-07-20 15:44:55 +0800

    initialized project

---
+++ dataimport/src/main/java/hehaorui/dataimport/DataimportApplication.java
@@ -0,0 +1,13 @@
+package hehaorui.dataimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataimportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataimportApplication.class, args);
+    }
+
+}
